362b4a4a39b5b4f4321f6477f979e1304a0e5a02
arthursl12 <arthursoutolima@gmail.com>
2023-01-12 10:03:29 -0300

    Solve exercise 6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    // Validation cases
+    if(b < a){
+        let c = a;
+        a = b;
+        b = c;
+    }else if (a < 0 || b < 0){
+        return "ERROR";
+    }else if(!Number.isInteger(a) || !Number.isInteger(b)){
+        return "ERROR";
+    }
 
+    // Sum
+    let sum=0;
+    for(let i=a; i<=b; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
