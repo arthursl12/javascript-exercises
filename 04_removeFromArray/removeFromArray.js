b656bcbe45230c351987f369eb3a0cfe62b793bd
arthursl12 <arthursoutolima@gmail.com>
2023-01-12 09:58:51 -0300

    Solve exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr,...args) {
+    let newArr = [];
+    let j = 0;
+    for(let i=0; i<arr.length; i++){
+        if(!args.includes(arr[i])){
+            newArr[j] = arr[i];
+            j++;
+        }
+    }
+    console.log(newArr)
+    return newArr;
 };
 
 // Do not edit below this line
