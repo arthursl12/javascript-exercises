c4f324261b8d91345574db01ab996254d07bb13c
arthursl12 <arthursoutolima@gmail.com>
2023-01-12 10:06:18 -0300

    Solve exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(y) {
+    if(y % 4 == 0){
+        if(y % 100 == 0 && y % 400 == 0){
+            return true;
+        }else if (y % 100 == 0){
+            return false;
+        }else{
+            return true; 
+        }
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line
