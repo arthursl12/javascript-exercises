a13719178230514de4d9a84ef5de0748b7677c10
arthursl12 <arthursoutolima@gmail.com>
2023-01-18 10:22:50 -0300

    Solve exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const reverseString = function(str) {
+    let rev = "";
+    for(let i=0; i<str.length; i++){
+        rev += str[str.length-i-1];
+    }
+    return rev;
+};
 
+
+const palindromes = function (str) {
+    str = str.replace(/[\p{P}$+<=>^`|~]/gu, '');    // remove punctuation
+    str = str.replace(/ /g,'');                     // remove spaces
+    str = str.toLowerCase();                        // lowercase everything
+
+    revStr = reverseString(str);
+    return str == revStr;
 };
 
 // Do not edit below this line
