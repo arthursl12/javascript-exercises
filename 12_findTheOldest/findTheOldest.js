afa4a3f87c27810a128708b427707cf5f3648269
arthursl12 <arthursoutolima@gmail.com>
2023-01-18 13:22:02 -0300

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    
+    // console.table(people)
+    let sorted = people.sort(function(a,b){
+        if (!a.yearOfDeath){
+            aYOD = (new Date()).getFullYear();
+        }else{
+            aYOD = a.yearOfDeath;
+        }
+        if (!b.yearOfDeath){
+            bYOD = (new Date()).getFullYear();
+        }else{
+            bYOD = b.yearOfDeath;
+        }
 
+        return (aYOD - a.yearOfBirth) > (bYOD - b.yearOfBirth) ? -1 : 1;
+    });
+            
+
+    return sorted[0];
 };
 
 // Do not edit below this line
