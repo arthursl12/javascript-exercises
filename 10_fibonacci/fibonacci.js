08e458cb8f93cb9f8ad521ee7cdd466f89f4ca4a
arthursl12 <arthursoutolima@gmail.com>
2023-01-18 10:26:44 -0300

    Solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0){
+        return "OOPS";
+    }else if (n < 2){
+        return 1;
+    }
 
+    let i1 = 1;
+    let i2 = 1;
+    for(let i=2; i<n; i++){
+        let next = i1 + i2;
+        i1 = i2;
+        i2 = next;
+    }
+    return i2;
 };
 
 // Do not edit below this line
